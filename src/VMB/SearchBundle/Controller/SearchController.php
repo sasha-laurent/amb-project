24f6198a8b2cea3ff2eb29c4cd59338b641775b4
Aris Tritas <a.tritas@gmail.com>
2015-08-12 20:04:12 +0200

    recherche v.02

--- src/VMB/SearchBundle/Controller/SearchController.php
+++ src/VMB/SearchBundle/Controller/SearchController.php
@@ -13,18 +13,32 @@ class SearchController extends Controller
 	 *
 	 * @Security("is_granted('IS_AUTHENTICATED_REMEMBERED')")
 	**/
-    public function indexAction(Request $request, $type, $query, $page=1)
+    public function indexAction(Request $request)
     {
+    	$query = $request->query->get("query");
+    	if(null === $query or empty($query)){
+    		return $this->redirectToRoute("vmb_presentation_browse", 
+    							$request->query->all());
+    	}
 
+    	$em = $this->getDoctrine()->getManager();
+    	$usr = $this->getUser();
+        
         /*
-         *
-         * Results + Counts 
-         *
+         * Results
         **/
+    	$pres_repo = $em->getRepository('VMBPresentationBundle:Presentation');
+    	$pres_results = $pres_repo->searchWithQuery($query, $usr);
+
+    	$ress_repo = $em->getRepository('VMBResourceBundle:Resource');
+    	$ress_results = $ress_repo->searchWithQuery($query, $usr);
+
         $render_opts = array(
+        	'query' => $query,
         	'backButtonUrl' => $this->get('vmb_presentation.previous_url')->getPreviousUrl($request),
-        	'nbPages' => 1,
-        	'page', $page);
-        return $this->render('VMBSearchBundle:Search:results.html.twig', $render_opts);
+        	'presentations' => $pres_results,
+        	'resources' => $ress_results);
+        return $this->render('VMBSearchBundle:Search:results.html.twig', 
+        			$render_opts);
     }
 }
