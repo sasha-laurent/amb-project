84ff7bc68b750116555190b6b9ab77bfbbbdbbc0
agcaroff <anne.caroff@telecom-bretagne.eu>
2016-02-25 18:25:42 +0100

    Rajout de deux fonctions pour compter le nombre de présentations visibles par l'utilisateur ou non

--- src/VMB/PresentationBundle/Entity/TopicRepository.php
+++ src/VMB/PresentationBundle/Entity/TopicRepository.php
@@ -42,4 +42,69 @@ class TopicRepository extends NestedTreeRepository
     	// Return total presentations count
 		return $count;
 	}
+        
+        /**
+	 * Number of presentations linked to each topic which are visible for one user.
+	 * TODO: Join on presentations table, parametrized by "default", "official", "public/personal" args
+	 ** $public=true, $official=true, $default='all', $user = null
+	 */
+	public function getVisiblePresentationsCounts($topic,$user)
+	{
+		$count = 0;
+                $total =0;
+		$query_res = 0;
+		if($topic != null)
+		{
+			try{ // Calculate own presentations count
+				$presentation_repo = $this->getEntityManager()->getRepository('VMBPresentationBundle:Presentation');
+				$qb = $presentation_repo->createQueryBuilder('p')->select('count(p)')
+				->where('p.topic = :tpc')->setParameter('tpc', $topic)
+                                ->andWhere('p.public = 1 OR p.owner = :user')->setParameter('user', $user);
+				$query_res = $qb->getQuery()->getSingleScalarResult();	
+				$count += $query_res;
+                                $total+=$query_res;
+
+			} catch (\Doctrine\ORM\NonUniqueResultException $e)
+			{
+				echo $e;
+			}
+
+            // Calculate topic's children presentations count
+			$children = $topic->getChildren();
+            foreach($children as $c){
+            	$query_res = $this->getVisiblePresentationsCounts($c,$user)[0];
+            	$count += $query_res;
+       		}
+    	}
+    	// Return total presentations count
+		return array($count,$total);
+	}
+        
+        /**
+	 * Number of presentations in a parent topic only
+	 * TODO: Join on presentations table, parametrized by "default", "official", "public/personal" args
+	 ** $public=true, $official=true, $default='all', $user = null
+	 */
+	public function getParentPresentationsCounts($topic,$user)
+	{
+		$count = 0;
+		$query_res = 0;
+		if($topic != null)
+		{
+			try{ // Calculate own presentations count
+				$presentation_repo = $this->getEntityManager()->getRepository('VMBPresentationBundle:Presentation');
+				$qb = $presentation_repo->createQueryBuilder('p')->select('count(p)')
+				->where('p.topic = :tpc')->setParameter('tpc', $topic)
+                                ->andWhere('p.public = 1 OR p.owner = :user')->setParameter('user', $user);
+				$query_res = $qb->getQuery()->getSingleScalarResult();	
+				$count += $query_res;
+
+			} catch (\Doctrine\ORM\NonUniqueResultException $e)
+			{
+				echo $e;
+			}
+    	}
+    	// Return total presentations count
+		return $count;
+	}
 }
